944e5e50484877e35139b13aca429fe11c19692c
Christian Hokayem <christian.hokayem@ieee.org>
2018-07-14 16:44:57 +0300

    # This is a combination of 3 commits. # The first commit's message is: Added canvas controller+jsp+js
    # This is the 2nd commit message:
    
    added wiring + backend/frontend communication
    
    # This is the 3rd commit message:
    
    Started serializer

--- src/main/java/com/iea/Application.java
+++ src/main/java/com/iea/Application.java
@@ -1,5 +1,6 @@
 package com.iea;
 
+import com.iea.serializer.Serializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- src/main/java/com/iea/controller/Index.java
+++ src/main/java/com/iea/controller/Index.java
@@ -1,15 +1,28 @@
 package com.iea.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class Index {
-
     @RequestMapping
-    public String index(Model model){
+    public String index(Model model) {
         return "index";
+    }
+
+    @RequestMapping("/canvas")
+    public String canvas(Model model) {
+        return "canvas";
+    }
+
+    @PostMapping("/canvas/update")
+    public void canvasUpdate(@RequestParam("components") String components, @RequestParam("connections") String connections) {
+        System.out.println(components);
+        System.out.println(connections);
     }
 }
