7cd648c128565e37d6bab2a451b78cb860f44c83
Christian Hokayem <christian.hokayem@ieee.org>
2018-07-18 14:47:23 +0300

    added wiring + backend/frontend communication

--- src/main/webapp/scripts/canvas.js
+++ src/main/webapp/scripts/canvas.js
@@ -1,6 +1,13 @@
 //counter used to assign unique IDs to clones
 let counter = 0;
 
+//array containing linked components' IDs
+let wiringList = [];
+let componentList = [];
+let wiring = false;
+let wiringTarget = null;
+
+let components = document.getElementById("components");
 //bind click event to adding components from "components" div in .JSP
 components.addEventListener("click", clickedComponent);
 
@@ -9,13 +16,27 @@ function clickedComponent(event)
 {
 	caller = event.target
 	
-	if (caller.id == "components") return; //ignore clicking the div's background
-	
-	alert("Created new: " + caller.id);
-	
-	oldComponent = caller;
-	newComponent = oldComponent.cloneNode();
-	newComponent.id = caller.id + counter++; 
+	if (caller.id == "components" || caller.id == "") return; //ignore clicking the div's background
+    if (caller.id == "wire")
+    {
+      if(wiring)
+      {
+        wiring = false;
+        wiringTarget = null;
+        document.body.style.cursor = 'auto';
+      }
+      else
+      {
+        wiring = true;
+        wiringTarget = null;
+        document.body.style.cursor = 'nesw-resize';
+      }
+      return;
+    }
+
+	newComponent = caller.cloneNode();
+	newComponent.id = caller.id + '-' + counter++;
+	componentList.push(newComponent.id);
 
 	//add events to the clones
 	//mouse down for dragging components
@@ -23,6 +44,7 @@ function clickedComponent(event)
     //right click for deleting components
 	newComponent.addEventListener("contextmenu", deleteComponent);
 	newComponent.ondragstart = function() {return false;}; //disable default drag event
+	newComponent.l
 	document.body.append(newComponent); //add cloned component to body
 };
 
@@ -30,6 +52,20 @@ function clickedComponent(event)
 function deleteComponent(event)
 {
 	event.preventDefault(); //prevents default context menu from opening
+    caller = event.target;
+    for(var i=0; i<wiringList.length;i++)
+    {
+      if (wiringList[i][0] == caller.id || wiringList[i][1] == caller.id)
+      {
+        wiringList[i].length = 0;
+      }
+    }
+
+    for(var i=wiringList.length-1; i>=0;i--)
+    {
+      if (wiringList[i].length == 0)
+        wiringList.splice(i,1);
+    }
 	event.target.remove(); //deletes component
 }
 
@@ -37,7 +73,36 @@ function deleteComponent(event)
 function moveComponent(event) {
   if (event.which != 1) //if not left click, ignore
 	return false;
+
   caller = event.target;
+
+  if (wiring)
+	{
+	    if (wiringTarget == null)
+	    {
+	        wiringTarget = caller;
+	        return false;
+	    }
+	    else if (wiringTarget == caller)
+	    {
+	        return false;
+	    }
+	    else
+	    {
+	        let connection = [wiringTarget.id, caller.id];
+	        wiringList.push(connection);
+	        alert("connected:" + wiringTarget.id + " to " + caller.id);
+	        wiring = false;
+	        wiringTarget = null;
+	        document.body.style.cursor = 'auto';
+	        return true;
+	    }
+	}
+
+  //record initial click offset from object edge for smoother dragging
+  let shiftX = event.clientX - caller.getBoundingClientRect().left;
+  let shiftY = event.clientY - caller.getBoundingClientRect().top;
+
   caller.style.position = 'absolute';
   caller.style.zIndex = 1000; //place dragged object on top of screen
 
@@ -47,8 +112,8 @@ function moveComponent(event) {
 
   //function used to move object center to mouse location
   function moveAt(pageX, pageY) {
-    caller.style.left = pageX - caller.offsetWidth / 2 + 'px';
-    caller.style.top = pageY - caller.offsetHeight / 2 + 'px';
+    caller.style.left = pageX - shiftX + 'px';
+    caller.style.top = pageY - shiftY + 'px';
   }
 
   //function called on mouse move to move object
@@ -63,6 +128,16 @@ function moveComponent(event) {
     document.removeEventListener('mousemove', onMouseMove); //delete mouse move event listener
     caller.onmouseup = null; //delete mouse up event listener
   };
-
 }
-  
+
+submitbutton.onclick = function(){
+
+let connectionsString = wiringList.toString();
+
+$.ajax({
+     type: 'POST',
+     url: "/canvas/update",
+     data: { name: "maincircuit", connections: connectionsString} });
+
+alert("Circuit Updated!");
+};
